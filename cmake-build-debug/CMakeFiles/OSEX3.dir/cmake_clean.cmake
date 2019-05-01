6f277481f4386c88f5e483c78b7c8ecd5f35f260
ofir.tziter <ofir.tziter@mail.huji.ac.il>
2019-05-01 09:20:12 +0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/OSEX3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OSEX3.dir/main.cpp.o"
+  "OSEX3.pdb"
+  "OSEX3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OSEX3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
