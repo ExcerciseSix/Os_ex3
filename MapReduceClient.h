e8bf8f6971a63421ead14e07c2e9127d46bf7b30
ofir.tziter <ofir.tziter@mail.huji.ac.il>
2019-05-01 18:17:11 +0300

    first commit

--- MapReduceClient.h
+++ MapReduceClient.h
@@ -56,14 +56,12 @@ class MapReduceClient {
 public:
 	// gets a single pair (K1, V1) and calls emit2(K2,V2, context) any
 	// number of times to output (K2, V2) pairs.
-	virtual void map(const K1* key, const V1* value, void* context)
-		const = 0;
+	virtual void map(const K1* key, const V1* value, void* context) const = 0;
 
 	// gets a single K2 key and a vector of all its respective V2 values
 	// calls emit3(K3, V3, context) any number of times (usually once)
 	// to output (K3, V3) pairs.
-	virtual void reduce(const IntermediateVec* pairs, void* context)
-		const = 0;
+	virtual void reduce(const IntermediateVec* pairs, void* context) const = 0;
 };
 
 
--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -1,3 +1,86 @@
 #include "MapReduceFramework.h"
+#include "Barrier.h"
 #include <pthread.h>
+#include <vector>
+#include <atomic>
 
+using namespace std;
+
+void* jobToExecute(void* context);
+
+class JobContext
+{
+public:
+	JobContext(int numOfThreads, int threadID,	JobState state,	Barrier* barrier, const MapReduceClient& client,
+			const InputVec& inputVec, OutputVec& outputVec, atomic<int>* atomic_counter) :
+			numOfThreads(numOfThreads), threadID(threadID), state(state), barrier(barrier), client(client),
+			inputVec(inputVec), outputVec(outputVec), atomic_counter(atomic_counter) {}
+	int numOfThreads;
+	int threadID;
+	JobState state;
+	Barrier* barrier;
+	const MapReduceClient& client;
+	const InputVec& inputVec;
+	OutputVec& outputVec;
+	atomic<int>* atomic_counter;
+};
+
+
+JobHandle startMapReduceJob(const MapReduceClient& client, const InputVec& inputVec,
+							OutputVec& outputVec, int multiThreadLevel) {
+	
+	pthread_t threads[multiThreadLevel];
+	vector<JobContext> contexts;
+	Barrier barrier(multiThreadLevel);
+	JobState state ={UNDEFINED_STAGE, 0};
+	atomic<int> atomic_counter(0);
+	
+	for (int i = 0; i < multiThreadLevel; ++i) {
+		contexts.push_back(JobContext(multiThreadLevel, i, state, &barrier, client, inputVec, outputVec, &atomic_counter));
+	}
+	
+	for (int i = 0; i < multiThreadLevel; ++i) {
+		pthread_create(&threads[i], NULL, jobToExecute, &contexts[i]);
+	}
+	
+	for (int i = 0; i < multiThreadLevel; ++i) {
+		pthread_join(threads[i], NULL);
+	}
+}
+
+
+void* jobToExecute(void* context)
+{
+	JobContext* jc = (JobContext*) context;
+	int old_value = (*(jc->atomic_counter))++;
+	jc->client.map(jc->inputVec[old_value].first, jc->inputVec[old_value].second, context);
+	
+	jc->barrier->barrier();
+	
+}
+
+
+void emit2(K2 *key, V2 *value, void *context)
+{
+
+}
+
+void emit3(K3 *key, V3 *value, void *context)
+{
+
+}
+
+void waitForJob(JobHandle job)
+{
+
+}
+
+void getJobState(JobHandle job, JobState *state)
+{
+	*state = ((JobContext*)job)->state;
+}
+
+void closeJobHandle(JobHandle job)
+{
+
+}
